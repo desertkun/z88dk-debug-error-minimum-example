33280d251bd4541a57eac5f8eef291df86bb4fe1
desertkun <desertkun@gmail.com>
2022-01-08 18:57:00 +0200

    check

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
 #include "header.h"
 #include <stdio.h>
 
+void check(const char* test, int* kek)
+{
+   printf("%s\n", test);
+}
+
 int main()
 {
+    int v = 20;
+    check("hello!", &v);
     int h = haha(13, 2);
     printf("result is: %d, supposed to be: 30\n", h);
     return h;
