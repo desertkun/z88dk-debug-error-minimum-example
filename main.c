e9dee7ba648a32bdf2d769318ce32c217d8c4fe9
desertkun <desertkun@gmail.com>
2022-01-09 20:38:32 +0200

    more examples

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include "header.h"
 #include <stdio.h>
 
-void check(const char* test, int* kek)
+static int haha55 = 22;
+
+void check(const char* test, int* kek, int value)
 {
+   static int wow = 12;
    printf("%s\n", test);
 }
 
 int main()
 {
     int v = 20;
-    check("hello!", &v);
+    check("hello!", &v, 50);
+    haha55 = 55;
     int h = haha(13, 2);
     printf("result is: %d, supposed to be: 30\n", h);
     return h;
