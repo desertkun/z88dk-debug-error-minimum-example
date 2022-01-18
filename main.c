17484ce80d93151c3269c80008fa4aba1f0de1a4
desertkun <desertkun@gmail.com>
2022-01-18 23:55:50 +0200

    more examples

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include "header.h"
 #include <stdio.h>
 
+struct child_t {
+  int c;
+};
+
 struct test_t {
   int vv1;
   char vv2;
+  struct child_t child;
 };
 
 static int haha222 = 222;
@@ -19,6 +24,7 @@ char* check(const char* test, struct test_t* kek, int value, int[] arr)
 
 int main()
 {
+    v.child.c = 54;
     v.vv1 = 20;
     check("hello!", &v, 50, haha55);
     haha55[1] = 55;
