ac45996dd1598cf033321fb59712d8115b9d6770
desertkun <desertkun@gmail.com>
2022-01-18 23:49:58 +0200

    more examples

--- main.c
+++ main.c
@@ -1,19 +1,27 @@
 #include "header.h"
 #include <stdio.h>
 
-static int haha55 = 22;
+struct test_t {
+  int vv1;
+  char vv2;
+};
 
-void check(const char* test, int* kek, int value)
+static int haha222 = 222;
+static int haha55[2] = {22, 11};
+struct test_t v;
+
+char* check(const char* test, struct test_t* kek, int value, int[] arr)
 {
    static int wow = 12;
-   printf("%s\n", test);
+   printf("%s %d\n", test, kek->vv1);
+   return "success";
 }
 
 int main()
 {
-    int v = 20;
-    check("hello!", &v, 50);
-    haha55 = 55;
+    v.vv1 = 20;
+    check("hello!", &v, 50, haha55);
+    haha55[1] = 55;
     int h = haha(13, 2);
     printf("result is: %d, supposed to be: 30\n", h);
     return h;
